d0725b2738c607323b060414c9abd8e0b0bf3de8
Jean Alvarez <116139695+JeanAlvarez21@users.noreply.github.com>
2025-01-03 23:35:50 -0500

    notifications_fix

--- resources/views/notification-center.blade.php
+++ resources/views/notification-center.blade.php
@@ -7,18 +7,37 @@
     <title>Centro de Notificaciones</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
+     .hero-section {
+            background: url('{{ asset('media/background_main.png') }}') no-repeat center center;
+            background-size: cover;
+            color: white;
+            padding: 100px 0;
+        }
+
+
+        .hero-overlay {
+            background-color: rgba(0, 0, 0, 0.5);
+            padding: 50px;
+            border-radius: 10px;
+        }
+
         .navbar {
             background-color: #FFD700;
+        }
+
+        .no-link {
+            text-decoration: none;
+            color: inherit;
         }
     </style>
 </head>
 
 <body>
-    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('media/logo.png') }}" alt="Logo" width="40" height="40">
+            <a class="navbar-brand" href="{{ url('home') }}">
+                <img src="{{ asset('media/logo.png') }}" alt="Logo" class="img-fluid"
+                    style="height: 6vh; max-height: 100%; width: auto;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +46,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio</a>
+                        <a class="nav-link active" href="#">Menú</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Productos</a>
@@ -41,11 +60,28 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contacto</a>
                     </li>
+                    @auth
+                        @if(Auth::user()->rol == 1)
+                            <li class="nav-item">
+                                <a href="/dashboard" class="nav-link no-link">Admin</a>
+                            </li>
+                        @elseif(Auth::user()->rol == 2)
+                            <li class="nav-item">
+                                <a href="/productos" class="nav-link no-link">Empleado</a>
+                            </li>
+                        @endif
+                    @endauth
                 </ul>
                 <div class="d-flex align-items-center">
-                    <a href="{{ route('login') }}">
-                        <img src="{{ asset('media/boton-usuario.png') }}" alt="Login/Register" width="30" height="30">
-                    </a>
+                    @auth
+                        <a href="{{ route('profile') }}">
+                            <img src="{{ asset('media/boton-usuario.png') }}" alt="Profile" width="30" height="30">
+                        </a>
+                    @else
+                        <a href="{{ route('login') }}">
+                            <img src="{{ asset('media/boton-usuario.png') }}" alt="Login/Register" width="30" height="30">
+                        </a>
+                    @endauth
                     <span class="mx-3">|</span>
                     <a href="{{ route('notificaciones') }}">
                         <img src="{{ asset('media/boton-notificaciones.png') }}" alt="Notificaciones" width="30"
